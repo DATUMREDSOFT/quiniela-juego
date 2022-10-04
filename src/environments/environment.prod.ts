a5e8ea45c0cf855e3072000be27a10da8125b6af
guzmanespinoza <luisespinoza.le109@gmail.com>
2022-10-04 10:00:38 -0600

    commit prd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  UrlAPi:'http://quiniela.eastus2.cloudapp.azure.com:8080',
+  UrlAPi:'https://quiniela.datumredsoft.com:8080',
   http:'https://',
 };
